0c240b54f6130ea479c50354de12442ca5d41bdd
Matias Pellegrineschi <pellegrineschi86@gmail.com>
2026-03-17 20:42:28 -0300

    feat: implementar flujo de reserva de turnos con selección de horarios

--- js/app.js
+++ js/app.js
@@ -10,17 +10,61 @@ function cargarHorarios() {
       lista.innerHTML = "";
 
       data.horarios_disponibles.forEach((hora) => {
-        const li = document.createElement("li");
 
-        li.classList.add("list-group-item");
+        const boton = document.createElement("button");
 
-        li.textContent = hora;
+        boton.classList.add("btn", "btn-outline-primary", "m-1");
 
-        lista.appendChild(li);
+        boton.textContent = hora;
+
+        //evento click
+        boton.addEventListener("click", ()=>{
+          reservarTurno(hora);
+        });
+
+        lista.appendChild(boton);
+        // const li = document.createElement("li");
+
+        // li.classList.add("list-group-item");
+
+        // li.textContent = hora;
+
+        // lista.appendChild(li);
       });
     })
 
     .catch((error) => {
       console.error("Error al cargar horarios:", error);
+    });
+}
+
+function reservarTurno(hora) {
+
+  const datos = {
+    usuario_id: 1,
+    profesional_id: 1,
+    fecha_hora: `2026-03-25 ${hora}:00`,      
+  };
+
+  fetch("http://localhost/sistema-turnos-api/api/turnos/crear.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datos)
+
+  })  
+    .then((response) => response.json())
+    
+    .then((data) => {
+      if (data.success) {
+        alert("Turno reservado con éxito");
+        
+      } else {
+        alert("Error al reservar turno: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error al reservar turno:", error);
     });
 }
